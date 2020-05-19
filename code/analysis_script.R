80557cea6771c119d2c65be01da546c4ee848e17
Jeff Wesner <jeff.wesner@usd.edu>
2020-05-19 13:43:29 -0500

    Update analysis_script.R

--- code/analysis_script.R
+++ code/analysis_script.R
@@ -1,4 +1,4 @@
-## ----echo=FALSE, message=FALSE, warning=FALSE, paged.print=FALSE---------------------------------------------------------
+## ----echo=FALSE, message=FALSE, warning=FALSE, paged.print=FALSE----------------------------------------------------------
 library(tidyverse)
 library(here)
 library(janitor)
@@ -16,7 +16,7 @@ library(brms)
 library(kableExtra)
 
 
-## ----echo=FALSE, fig.height=2, message=FALSE, warning=FALSE, paged.print=FALSE-------------------------------------------
+## ----echo=FALSE, fig.height=2, message=FALSE, warning=FALSE, paged.print=FALSE--------------------------------------------
 ggplot() +
   annotate("text", x = 0.5, y = 0.7,
                    label = "(y_cumulative[i]) %~% italic(N)(mu[i], sigma)", parse = T, family = "serif") +
@@ -36,7 +36,7 @@ ggplot() +
   theme(text = element_text(family = "serif"))
 
 
-## ----include=FALSE, cache = F--------------------------------------------------------------------------------------------
+## ----include=FALSE, cache = F---------------------------------------------------------------------------------------------
 sd_pop <- 903027 #from here: https://worldpopulationreview.com/states/south-dakota-population/
 
 sd_covid_data <- read.csv(here::here("data/data_kelo.csv")) %>%
@@ -160,7 +160,7 @@ predict_dailyhosp <- predict(fit_weib, newdata = newdata, summary = F)  %>%
             low95 = quantile(daily_total, probs = 0.025),
             low50 = quantile(daily_total, probs = 0.25))
 
-## ----echo=FALSE, fig.height=9, fig.width=8, message=FALSE, warning=FALSE, paged.print=FALSE------------------------------
+## ----echo=FALSE, fig.height=9, fig.width=8, message=FALSE, warning=FALSE, paged.print=FALSE-------------------------------
 # Make plots
 
 cumulative_plot <- predict_cumulative %>% 
@@ -202,7 +202,7 @@ both_plot_weib <- plot_grid(cumulative_plot, active_plot, ncol = 1)
 both_plot_weib
 
 
-## ----echo=FALSE, message=FALSE, warning=FALSE, paged.print=FALSE---------------------------------------------------------
+## ----echo=FALSE, message=FALSE, warning=FALSE, paged.print=FALSE----------------------------------------------------------
 #prior v post
 
 posts <- posterior_samples(fit_weib) %>% as_tibble() %>% mutate(iter = 1:nrow(.))
@@ -244,4 +244,209 @@ all_posts %>%
        title = "Prior versus Posterior",
        subtitle = "Difference in models indicates the influence of the data") +
   NULL
+
+
+## ----echo=FALSE, message=FALSE, warning=FALSE, paged.print=FALSE, include = F---------------------------------------------
+library(tidyverse)
+library(here)
+library(janitor)
+library(viridis)
+library(deSolve)
+library(scales)
+library(lubridate)
+library(viridis)
+library(readr)
+library(readxl)
+library(ggrepel)
+library(cowplot)
+library(viridis)
+library(brms)
+library(kableExtra)
+
+
+cum_hosp <- read_csv("data/cum_hosp.csv", na = c("", ".")) %>% mutate(date = mdy(date_hosp))
+sd_county_pops <- read_csv("data/sd_county_pops.csv") %>% 
+  select(-X1) 
+# 
+# new_data <- tibble(date_hosp = ymd("2020-05-15"),
+#                    group = c("Minnehaha County", "Rest of South Dakota"),
+#                    n = NA,
+#                    cum_hosp = c(228, 76))
+
+group_pops <- sd_county_pops %>% 
+  mutate(group = case_when(county == "Minnehaha" ~ "Minnehaha County",
+                           TRUE ~ "Rest of South Dakota")) %>%
+  group_by(group) %>% 
+  summarize(group_pop = sum(total_pop))
+
+
+# beta_parms <- function(mean, sd) {
+#   out <- tibble(alpha = (mean^2 - mean^3 - mean*sd^2)/sd^2,
+#                 beta = (mean - 2*mean^2 + mean^3 - sd^2 + mean*sd^2)/sd^2)
+#   
+#   plot <- plot(density(rbeta(1000, out[[1]], out[[2]])))
+#   return(out)
+#   return(plot)
+# }
+
+# beta_parms(0.0004, 0.0001)
+# fit_weib <- brm(bf(cum_prop_hosp|trunc(lb = 0) ~ (ult*date_num^omega)/(theta^omega + date_num^omega),
+#                    ult ~ 1, omega ~ 1, theta ~ 1,
+#                    nl = TRUE),
+#                 data = sd_covid_data,
+#                 # family = gaussian(),
+#                 family = gaussian(),
+#                 prior = c(
+#                   prior(beta(2.76, 549), nlpar = "ult"), #original_prior
+#                   prior(gamma(4, 0.04), nlpar = "theta"),
+#                   prior(normal(4, 0.4), nlpar = "omega")),
+#                 iter = 4000,
+#                 sample_prior = "yes")
+minn_hosp <- cum_hosp %>% filter(group == "Minnehaha County") %>% rename(cum_prop_hosp = prop_hosp) %>% mutate(date_num = date_num - 18341)
+rest_hosp <- cum_hosp %>% filter(group == "Rest of South Dakota") %>% rename(cum_prop_hosp = prop_hosp) %>% mutate(date_num = date_num - 18329)
+
+
+fit_priorday <- readRDS(here::here("code/fit_weib.rds"))
+
+fit_minn <- update(fit_priorday, newdata = minn_hosp) #re-run the model above with today's data (this shortcut just prevents the need to recompile the model)
+# post_minn <- posterior_samples(fit_minn)
+# beta_parms(mean = mean(post_minn$b_ult_Intercept), sd = 2*sd(post_minn$b_ult_Intercept)) #prior for rest of sd
+
+fit_rest <- update(fit_priorday, newdata = rest_hosp, prior = c(
+                  prior(beta(679, 476819), ub = 1, lb = 0, nlpar = "ult"), # use prior from Minnehaha for the max but with 2x wider sd
+                  prior(gamma(4, 0.04), lb = 0, nlpar = "theta"),
+                  prior(normal(4, 0.4), nlpar = "omega")))
+
+# priors come from here:
+# ult is the Asymptote: Prior assumes asymptote with mean of 4000 cumulative total people ever hospitalized with wide sd of 2000 - parameterized as beta. That is
+# based on NYC's curve, in which a total of 0.45% of their population was ever hospitalized.
+
+# theta is the inflection point - Prior assumes mean of 100 with sd of 30 days - parameterized as gaussian. 
+# omega is the slope of inflection point - Prior assumes mean of 50 with sd of 20 - parameterized as gamma.
+
+
+## ----echo=FALSE, message=FALSE, warning=FALSE, paged.print=FALSE----------------------------------------------------------
+#predictions up to 200 days after the first hospitalization
+newdata = tibble(date_num = 0:300)
+
+predict_cumulative_minn <- predict(fit_minn, newdata = newdata, probs = c(0.025, 0.975, 0.25, 0.75))  %>% 
+  as_tibble() %>% 
+  clean_names() %>% 
+  mutate(date_num = newdata$date_num,
+         date = as_date(date_num + 18341),
+         method = "predicted",
+         group = "Minnehaha County") 
+
+predict_cumulative_rest <- predict(fit_rest, newdata = newdata, probs = c(0.025, 0.975, 0.25, 0.75))  %>% 
+  as_tibble() %>% 
+  clean_names() %>% 
+  mutate(date_num = newdata$date_num,
+         date = as_date(date_num + 18329),
+         method = "predicted",
+         group = "Rest of South Dakota") 
+
+
+all_cumulative <- bind_rows(predict_cumulative_minn, predict_cumulative_rest)
+
+#make predictions of daily hospital needs
+days_in_hosp <- 10 #adjusted to visually match actual active hosps
+#
+predict_dailyhosp_minn <- predict(fit_minn, newdata = newdata, summary = F)  %>%
+  as_tibble() %>%
+  mutate(iter = 1:nrow(.)) %>%
+  gather(key, value, -iter) %>%
+  mutate(date_num = 18341 + parse_number(key),
+         date = as_date(date_num)) %>%
+  arrange(iter, date_num) %>%
+  group_by(iter) %>%
+  mutate(daily_total = value - lag(value, days_in_hosp)) %>%
+  group_by(date_num) %>%
+  drop_na(daily_total) %>%
+  mutate(daily_total = daily_total) %>%
+  group_by(iter, date) %>%
+  filter(!any(daily_total < 0)) %>%
+  group_by(date) %>%
+  summarize(median = median(daily_total),
+            mean = mean(daily_total),
+            sd = sd(daily_total),
+            high95 = quantile(daily_total, probs = 0.975),
+            high50 = quantile(daily_total, probs = 0.75),
+            low95 = quantile(daily_total, probs = 0.025),
+            low50 = quantile(daily_total, probs = 0.25))
+
+
+
+predict_dailyhosp_rest <- predict(fit_rest, newdata = newdata, summary = F)  %>%
+  as_tibble() %>%
+  mutate(iter = 1:nrow(.)) %>%
+  gather(key, value, -iter) %>%
+  mutate(date_num = 18328 + parse_number(key),
+         date = as_date(date_num)) %>%
+  arrange(iter, date_num) %>%
+  group_by(iter) %>%
+  mutate(daily_total = value - lag(value, days_in_hosp)) %>%
+  group_by(date_num) %>%
+  drop_na(daily_total) %>%
+  mutate(daily_total = daily_total) %>%
+  group_by(iter, date) %>%
+  filter(!any(daily_total < 0)) %>%
+  group_by(date) %>%
+  summarize(median = median(daily_total),
+            mean = mean(daily_total),
+            sd = sd(daily_total),
+            high95 = quantile(daily_total, probs = 0.975),
+            high50 = quantile(daily_total, probs = 0.75),
+            low95 = quantile(daily_total, probs = 0.025),
+            low50 = quantile(daily_total, probs = 0.25))
+
+all_daily <- bind_rows(predict_dailyhosp_minn %>% mutate(group = "Minnehaha County"),
+                       predict_dailyhosp_rest %>% mutate(group = "Rest of South Dakota"))
+
+
+
+# Make plots
+cumulative_plot <- all_cumulative %>% 
+  left_join(group_pops) %>% 
+  ggplot(aes(x = date, y = estimate*group_pop)) +
+  geom_line(aes(color = group)) +
+  geom_ribbon(aes(ymin = q2_5*group_pop, ymax = q97_5*group_pop, fill = group), alpha = 0.2 ) +
+  geom_ribbon(aes(ymin = q25*group_pop, ymax = q75*group_pop, fill = group), alpha = 0.2 ) +
+  geom_point(data = cum_hosp, aes(x = date, y = cum_hosp, fill = group),
+             shape = 21) +
+  # facet_wrap(~ group, scales = "free") +
+  theme_classic() +
+  scale_x_date() +
+  # scale_x_date(date_breaks = "months" , date_labels = "%b") +
+  # ylim(0, 1500) +
+  labs(y = "Cumulative ever hospitalized",
+       title = "Weibull model results",
+       subtitle = "Dots = data. Shading = 50% and 95% prediction intervals") +
+  NULL
+
+# cumulative_plot
+
+
+active_plot <- all_daily %>% 
+  left_join(group_pops) %>% 
+  ggplot() +
+  geom_line(aes(x = date, y = median*group_pop, color = group)) +
+  geom_ribbon(aes(x = date, ymin = low95*group_pop, ymax = high95*group_pop, fill = group), alpha = 0.2) +
+  geom_ribbon(aes(x = date, ymin = low50*group_pop, ymax = high50*group_pop, fill = group), alpha = 0.2) +
+  # geom_point(data = all_hosp, aes(x = date, y = hospitalized_currently),
+  #            shape = 21, fill = "yellow") +
+  # scale_x_date(date_breaks = "months" , date_labels = "%b") +
+  theme_classic() +
+  # geom_hline(yintercept = all_beds, color = "dodgerblue") +
+  # annotate("text", x = as.Date("2020-04-10"), y = 2960, label = "All Hospital Beds\nin SD") +
+  labs(y = "Active hospitalizations",
+       title = "Predicting active hospitalizations from Weibull model",
+       subtitle = "Shading = 50% and 95% prediction intervals") +
+  # coord_cartesian(ylim = c(0,100)) +
+  NULL
+# active_plot
+
+both_plot_group <- plot_grid(cumulative_plot, active_plot, ncol = 1, align = "v")
+
+both_plot_group
+ggsave(both_plot_group, file = "plots/both_plot_group.jpg", width = 7, height = 7)
 
